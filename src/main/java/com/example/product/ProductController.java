82cb246a293426be5219ed4d6eca7b46273f8b7d
Hilyaturrizqiyah <hilyaturrizqiyah00@gmail.com>
2024-05-05 06:57:12 +0700

    [ADD] fitur show product

---
+++ src/main/java/com/example/product/ProductController.java
@@ -0,0 +1,9 @@
+package com.example.product;
+
+public class ProductController {
+    // @GetMapping("/products")
+    // @ResponseBody
+    public String getProduct() {
+        return "Product details";
+    }
+}
